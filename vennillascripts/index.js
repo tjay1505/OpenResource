fd1be72f78951c6acb2bd9f6bcf797d5862b7cb6
tjay1505 <119523945+tjay1505@users.noreply.github.com>
2023-03-04 00:22:58 +0530

    DNB - complete section - half finished

---
+++ vennillascripts/index.js
@@ -0,0 +1,12 @@
+function DNBtoggle(){
+    const barout = document.getElementById('DNBAR');
+    barout.style.width ='6em';
+    barout.style.height ='6em';
+    
+
+}
+function section(){
+    var borderof =document.getElementById('') ;
+    console.log(borderof);
+
+}
